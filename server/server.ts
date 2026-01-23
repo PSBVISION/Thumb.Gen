3727543e91d0525a83a82fef4872170ca53ed698
PSBVISION <punitsinghbisht1529@gmail.com>
2026-01-23 21:19:46 +0530

    refactor: clean up server.ts formatting and improve session cookie settings

--- server/server.ts
+++ server/server.ts
@@ -1,52 +1,65 @@
-import express, { Request, Response } from 'express';
-import 'dotenv/config'
-import cors from 'cors';
-import connectDB from './configs/db.js';
-import session from 'express-session';
-import MongoStore from 'connect-mongo';
-import AuthRouter from './routes/AuthRoutes.js';
-import ThumbnailRouter from './routes/ThumbnailRoutes.js';
-import UserRouter from './routes/UserRoutes.js';
+import express, { Request, Response } from "express";
+import "dotenv/config";
+import cors from "cors";
+import connectDB from "./configs/db.js";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import AuthRouter from "./routes/AuthRoutes.js";
+import ThumbnailRouter from "./routes/ThumbnailRoutes.js";
+import UserRouter from "./routes/UserRoutes.js";
 
 declare module "express-session" {
-    interface SessionData{
-        isLoggedIn: boolean;
-        userId: string;
-    }
+  interface SessionData {
+    isLoggedIn: boolean;
+    userId: string;
+  }
 }
 
 await connectDB();
 
 const app = express();
-
-app.use(session({
+app.set("trust proxy", 1);
+app.use(
+  session({
     secret: process.env.SESSION_SECRET as string,
     resave: false,
     saveUninitialized: false,
-    cookie:{maxAge: 1000 * 60 * 60 * 24 * 7},
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production"?"none":"lax",
+      path: "/",
+    },
     store: MongoStore.create({
-        mongoUrl: process.env.MONGODB_URI as string,
-        collectionName: 'sessions'
-    })
-}))
+      mongoUrl: process.env.MONGODB_URI as string,
+      collectionName: "sessions",
+    }),
+  }),
+);
 
-app.use(cors({
-    origin: ['http://localhost:5173','http://localhost:8080'],
-    credentials: true
-}))
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:8080",
+      "https://thumbgen.psbvision.engineer",
+    ],
+    credentials: true,
+  }),
+);
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Server is Live!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Server is Live!");
 });
-app.use('/api/auth', AuthRouter)
-app.use('/api/thumbnail', ThumbnailRouter)
-app.use('/api/user', UserRouter)
-
+app.use("/api/auth", AuthRouter);
+app.use("/api/thumbnail", ThumbnailRouter);
+app.use("/api/user", UserRouter);
 
 const port = process.env.PORT;
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
